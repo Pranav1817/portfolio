02e5234cf9886859f1825b82877776ef6d417fe2
Chaitanya637 <87860944+Pranav1817@users.noreply.github.com>
2025-02-24 19:20:08 +0530

    second commit

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -7,15 +7,15 @@ export default function Navbar() {
   const [isOpen, setIsOpen] = useState(false);
 
   return (
-    <nav className="bg-white/60 shadow-md sticky backdrop:blur-3xl top-0 z-50">
+    <nav className="sticky top-0 z-50 backdrop-blur-sm">
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div className="flex justify-between h-16">
           <div className="flex-shrink-0 flex items-center">
-            <Link href="/" className="text-2xl font-bold text-blue-500">
+            <Link href="/" className="text-2xl font-bold text-blue-500 tracking-tight">
               Pranav
             </Link>
           </div>
-          <div className="hidden md:flex items-center space-x-4">
+          <div className="hidden md:flex items-center space-x-4 font-bold">
             <Link href="/" className="text-black hover:text-gray-900">
               Home
             </Link>
